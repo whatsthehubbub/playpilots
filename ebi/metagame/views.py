b27080e60b9b5b66a36a19252531578fc83ddb02
Alper Çugun <github@alper.nl>
2010-09-16 13:28:45 +0000

    adde staging for stereoscoop '

--- ebi/metagame/views.py
+++ ebi/metagame/views.py
@@ -199,6 +199,9 @@ def game_detail(request, slug):
         
         convars['races'] = Kippenrace.objects.all().order_by('raceid')
         convars['riders'] = Kippenrijder.objects.all().order_by('time', 'raceid')
+        
+    if game.slug == 'de-stereoscoop':
+        templateName = 'metagame/game_detail_stereoscoop.html'
     
     return render_to_response(templateName, convars, context_instance=RequestContext(request))
     
