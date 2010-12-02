569b6f44ab3bdf29cf24beee75baf2cc84d1bcd6
Alper Çugun <github@alper.nl>
2010-12-02 22:12:06 +0000

    dumb  bug

--- ebi/bandjesland/views.py
+++ ebi/bandjesland/views.py
@@ -57,4 +57,4 @@ def special_occurrences(request):
     
     occurrenceTimes = BandjeslandSpecialOccurrence.objects.filter(session__label=sessionLabel).filter(special=special).order_by('time').values('time')
     
-    return HttpResponse(json.dumps([(occ['time']-session.start).seconds for occ in occurrenceTimes]), mimetype='text/plain')
+    return HttpResponse(json.dumps([(session.start-occ['time']).seconds for occ in occurrenceTimes]), mimetype='text/plain')
